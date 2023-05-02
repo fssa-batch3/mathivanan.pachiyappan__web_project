5d6a2519054d9ef59ab32cd9ab251d979767da9d
NSP.Mathi <116268923+mathivanan8@users.noreply.github.com>
2023-05-02 09:28:15 +0530

    Update Features

--- assets/header.js
+++ assets/header.js
@@ -1,4 +1,4 @@
-let rootPath = window.location.origin;
+const rootPath = window.location.origin;
 
 const beforeLogin = `<header>
 <div class="header">
@@ -11,14 +11,14 @@ const beforeLogin = `<header>
             <li>
                 <form>
                     <input type="text" placeholder="Search...." />
-                    <span><i class="fa fa-search" style="color: #000;"></i></span>
+                    <span><i class="fa fa-search" style="color: #000; background-color: white"></i></span>
                 </form>
             </li>
             <li><a href="./pages/Reg/2_login.html" id="login_btn">Login</a></li>
         </ul>
     </div>
 </div>
-</header>`
+</header>`;
 
 const AfterLogin = `<header>
 <div class="header">
@@ -27,7 +27,7 @@ const AfterLogin = `<header>
     <div class="navigation_link">
         <ul>
             <form>
-                <input type="text" placeholder="Search...." /><span><i class="fa fa-search" style="color: #000;"></i></span>
+                <input type="text" placeholder="Search...." /><span><i class="fa fa-search" style="color: #000; background-color: white; padding: 0.2rem;"></i></span>
               </form>
             </li>
             <li id="med-blk"><a href="${rootPath}/pages/user_services/4_creat_post.html">Create</a></li>
@@ -52,39 +52,45 @@ const AfterLogin = `<header>
         </ul>
     </div>
 </div>
-</header>`
-
+</header>`;
 
 const loginUser = JSON.parse(localStorage.getItem("email_ID"));
 // console.log(loginUser);
 if (loginUser) {
-    document.body.insertAdjacentHTML("afterbegin", AfterLogin);
-    const userLoginElement = document.getElementById("login_btn");
-    userLoginElement?.addEventListener("click", () => document.body.innerHTML = beforeLogin);
-    const userLogoutElement = document.getElementById("logout_btn");
-    userLogoutElement?.addEventListener("click", () => {
-        localStorage.removeItem("email_ID");
-        document.body.innerHTML = beforeLogin;
-    });
-} else {
-    document.body.insertAdjacentHTML("afterbegin", beforeLogin);
-    const userLoginElement = document.getElementById("logout_btn");
-    userLoginElement?.removeEventListener("click", () => document.body.innerHTML = AfterLogin);
+  document.body.insertAdjacentHTML("afterbegin", AfterLogin);
+  const userLoginElement = document.getElementById("login_btn");
+  userLoginElement?.addEventListener(
+    "click",
+    () => (document.body.innerHTML = beforeLogin)
+  );
+  const userLogoutElement = document.getElementById("logout_btn");
+  userLogoutElement?.addEventListener("click", () => {
     localStorage.removeItem("email_ID");
+    document.body.innerHTML = beforeLogin;
+  });
+} else {
+  document.body.insertAdjacentHTML("afterbegin", beforeLogin);
+  const userLoginElement = document.getElementById("logout_btn");
+  userLoginElement?.removeEventListener(
+    "click",
+    () => (document.body.innerHTML = AfterLogin)
+  );
+  localStorage.removeItem("email_ID");
 }
 
-let userRecords = JSON.parse(localStorage.getItem("user_records"));
-let profileImage = userRecords.find((e) => e.user_email == loginUser)?.user_profile;
-let profImg = document.getElementById("profile_img");
-profImg.style.backgroundImage = "url(" + profileImage + ")";
+const userRecords = JSON.parse(localStorage.getItem("user_records"));
+const profileImage = userRecords.find(
+  (e) => e.user_email == loginUser
+)?.user_profile;
+const profImg = document.getElementById("profile_img");
+profImg.style.backgroundImage = `url(${profileImage})`;
 
+const profileLink = document.getElementById("drop_down");
 
-let profileLink = document.getElementById("drop_down");
-
-let userData = JSON.parse(localStorage.getItem("email_ID"));
-let url = `${rootPath}/pages/Myprofile/6_profile.html?user_ID=${userData}`;
-let aTag = document.createElement("a");
-let iTag = document.createElement("i");
+const userData = JSON.parse(localStorage.getItem("email_ID"));
+const url = `${rootPath}/pages/Myprofile/6_profile.html?user_ID=${userData}`;
+const aTag = document.createElement("a");
+const iTag = document.createElement("i");
 
 aTag.setAttribute("href", url);
 iTag.classList.add("fa", "fa-user-circle-o");
@@ -93,39 +99,45 @@ aTag.appendChild(iTag);
 aTag.appendChild(document.createTextNode("Profile"));
 
 // Create the div element
-const divElement = document.createElement('div');
-divElement.classList.add('nav_main');
+const divElement = document.createElement("div");
+divElement.classList.add("nav_main");
 
 // Create the anchor element and set its href attribute
-const anchorElement = document.createElement('a');
-anchorElement.setAttribute("href", `${rootPath}/pages/home/3_home.html?user_ID=${userData}`);
+const anchorElement = document.createElement("a");
+anchorElement.setAttribute(
+  "href",
+  `${rootPath}/pages/home/3_home.html?user_ID=${userData}`
+);
 
 // Create the "Venom" text and add it as a child of the anchor element
-const spanElement = document.createElement('span');
-spanElement.textContent = 'nom';
-anchorElement.appendChild(document.createTextNode('Ve'));
+const spanElement = document.createElement("span");
+spanElement.textContent = "nom";
+anchorElement.appendChild(document.createTextNode("Ve"));
 anchorElement.appendChild(spanElement);
 
 // Add the anchor element as a child of the div element
-const paragraphElement = document.createElement('p');
+const paragraphElement = document.createElement("p");
 paragraphElement.appendChild(anchorElement);
 divElement.appendChild(paragraphElement);
 
 document.querySelector("div.header").prepend(divElement);
 
-let notific_card = JSON.parse(localStorage.getItem("comment_records"));
-let notific_length = document.getElementById("notific");
+const notific_card = JSON.parse(localStorage.getItem("comment_records"));
+const notific_length = document.getElementById("notific");
 notific_length.innerText = notific_card.length;
 if (notific_card.length <= 0) {
-    notific_length.style.display = 'none';
+  notific_length.style.display = "none";
 }
 
-let editProfile = document.getElementById("drop_down");
+const editProfile = document.getElementById("drop_down");
 
-let a_Tag = document.createElement("a");
-let i_tag = document.createElement("i");
+const a_Tag = document.createElement("a");
+const i_tag = document.createElement("i");
 
-a_Tag.setAttribute("href", `${rootPath}/pages/Myprofile/11_edit_profile.html?user_ID=${userData}`);
+a_Tag.setAttribute(
+  "href",
+  `${rootPath}/pages/Myprofile/11_edit_profile.html?user_ID=${userData}`
+);
 i_tag.classList.add("fa", "fa-gear");
 i_tag.style.fontSize = "15px";
 a_Tag.appendChild(i_tag);
